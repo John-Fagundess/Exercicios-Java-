905b1eb96a98c956e085ffa9bb6ec6f7d5843a72
John-Fagundess <johnatan.worktool@gmail.com>
2026-09-16 22:35:15 -0300

    Exercicio 1

---
+++ ex1/src/main/java/com/mycompany/ex1/Ex1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex1;
+
+/**
+ *
+ * @author 46341100833
+ */
+public class Ex1 {
+
+    public static void main(String[] args) {
+        
+    int inteiro = 5;
+    
+    double doublee = 5.98;
+    
+    char charr = 'F';
+    
+    boolean bolean = true;
+        
+        
+    System.out.printf("valor inteiro e %d. valor de double e %f. valor de char e %c. valor de boolean e %s " , inteiro, doublee, charr, bolean); 
+        
+    }
+}
